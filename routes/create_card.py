dc0ff5938b7a892f32a285201598c93634b67bea
HP657 <junlee5070@gmail.com>
2024-03-01 17:28:12 +0900

    회원가입 로직

--- routes/create_card.py
+++ routes/create_card.py
@@ -1,5 +1,40 @@
-from .PIL_def import PIL_def
+from PIL import Image, ImageDraw, ImageFont
+import base64
+import io
 
-def create_card():
-    card = PIL_def.set_card()
-    return card
+def encoding(image):
+    img_byte_array = io.BytesIO()
+    image.save(img_byte_array, format='PNG')
+    img_byte_array.seek(0)
+
+    encoded_data = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
+    return encoded_data
+
+def decoding(image):
+    image = base64.b64decode(image)
+    return image
+    
+
+def set_card():
+    W = 1050
+    H = 600
+    bg_color = 'rgb(255, 255, 255)'
+
+    image =Image.new('RGB', (W, H), color = bg_color)
+
+    encoded_data = encoding(image)
+    return encoded_data
+
+
+
+# font1 = ImageFont.truetype('BareunBatangB.ttf', size=25)
+# font2 = ImageFont.truetype('BareunBatangB.ttf', size=35)
+# draw.text((50, 300), 직군, font=font1, fill='#0000')
+# draw.text((50, 340), f'{이름}│{영어이름}', font=font2, fill='#0000')
+
+# draw.text((50, 450), f'TEL│{전화번호}', font=font1, fill='#0000')
+# draw.text((50, 480), 'GitHub│@HP657', font=font1, fill='#0000')
+# draw.text((50, 510), 'E-mail│junlee5070@gmail.com', font=font1, fill='#0000')
+
+
+
--- routes/main.py
+++ routes/main.py
@@ -1,13 +1,11 @@
 from flask import *
 from pool.database import DB
 import os
-from .create_card import create_card
+from .create_card import set_card
 
 from dotenv import load_dotenv
 
 load_dotenv()
-
-
 
 
 main_bp = Blueprint('main_bp', __name__)
@@ -24,11 +22,35 @@ db = DB(
 def main():
     return render_template('index.html')
 
+
+@main_bp.route('/signupa', methods=["GET", "POST"])
+def signup():
+    message = None
+
+    if request.method == "POST":
+        name = request.form['name']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        share_password = request.form['share_password']
+        confirm_share_password = request.form['confirm_share_password']
+
+        if password != confirm_password:
+            message = "비밀번호가 일치하지 않습니다."
+        elif share_password != confirm_share_password:
+            message = "공유 비밀번호가 일치하지 않습니다."
+        else:
+            # 여기서 회원가입 로직을 처리할 수 있습니다.
+            print(name, password, confirm_password, share_password, confirm_share_password)
+            return redirect(url_for('main_bp.main'))
+
+    return render_template('signup.html', message=message)
+
+
 @main_bp.route('/login')
 def login():
     pass
 
 @main_bp.route('/aa')
 def aa():
-    encoded_image = create_card()
+    encoded_image = set_card()
     return render_template('view_card.html', encoded_image=encoded_image)
