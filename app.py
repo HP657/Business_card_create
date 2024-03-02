46bbae35c5508c09599bdee379548122c6fe0e06
HP657 <junlee5070@gmail.com>
2024-03-02 18:02:39 +0900

    jwt 1시간 기한 설정

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from routes.main import main_bp
 import os
+from datetime import timedelta
 
 app = Flask(__name__)
 
 # JWT 설정
 app.config['JWT_SECRET_KEY'] = os.getenv("KEY")
 app.config['SECRET_KEY'] = os.getenv("KEY")
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
 
 # 블루프린트 등록
 app.register_blueprint(main_bp)
